52fcd3c052000c9f6acf2de74903a6692c6799d0
Shubham kumar <raoneface@gmail.com>
2026-07-21 13:43:28 +0530

    circular dependency introduce when two object depend each other at same time

---
+++ Day6/CircularDependenciesDemo/src/main/java/in/strikes/OrderService.java
@@ -0,0 +1,24 @@
+package in.strikes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderService {
+
+    PaymentService paymentService;
+
+    @Autowired
+    OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public void orderPlaced() {
+        paymentService.pay();
+        System.out.println("Order Placed");
+    }
+
+    public void getOrderDetails(){
+        System.out.println("Order details");
+    }
+}
---
+++ Day6/CircularDependenciesDemo/src/main/java/in/strikes/PaymentService.java
@@ -0,0 +1,22 @@
+package in.strikes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentService {
+    OrderService orderService;
+
+    @Autowired
+    public PaymentService(OrderService orderService){
+        this.orderService = orderService;
+    }
+
+
+    public void pay(){
+        System.out.println("Payment done");
+        orderService.getOrderDetails();
+    }
+
+
+}
